033942350b6a737f973188067075fabc99cad2a8
Martin BRANDEL <martin.brandel@Digital-Grenoble.local>
2022-01-24 14:30:19 +0100

    first commit

---
+++ item.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+
+<?php
+    $nom = "martin";
+    $prix = 3500;
+    $image = "../mamp_logo.png";
+    echo "$nom";
+    echo "$prix";
+    echo "<img src='$image' alt=''>";
+
+?>
+
+</body>
+</html>
+
