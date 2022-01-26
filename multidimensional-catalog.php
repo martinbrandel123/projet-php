6bbe5e80c0eb87951dc7aa8240e60aa389f9a03f
martin brandel <martin.brandel123@gmail.com>
2022-01-26 11:46:51 +0100

    validation Variables/Conditions/Boucle/Tableaux

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -6,27 +6,30 @@ $products = [
         "price" => 170,
         "weight" => 100,
         "discount" => 20,
-        "picture_url" => "picofshoenikeair.png",
+        "picture_url" => "pictureofshoenikeair.png",
     ],
     "niketerre270" => [
         "name" => "niketerre270",
         "price" => 270,
         "weight" => 220,
         "discount" => 55,
-        "picture_url" => "picofshoenikeair.png",
+        "picture_url" => "pictureofshoenikeair.png",
     ],
     "nikefeu270" => [
         "name" => "nikefeu270",
         "price" => 666,
         "weight" => 62,
         "discount" => 10,
-        "picture_url" => "picofshoenikeair.png",
+        "picture_url" => "pictureofshoenikeair.png",
     ],
 ];
 
-echo '<pre>';
-print_r($products);
-echo '</pre>';
 
-
+  foreach($products as $key => $shoes) {
+      echo "Model ".$key."<br>";
+      foreach($shoes as $shoe) {
+          echo $shoe."<br>";
+      }
+      echo "<hr>";
+  }
 ?>
--- simple-catalog.php
+++ simple-catalog.php
@@ -4,12 +4,18 @@ $products = array("iPhone", "iPad", "iMac");
 $productsLastIndex = count($products) - 1;
 
 // Trier le tableau par ordre alphabétique croissant
-sort($products);
-print_r($products);
+//sort($products);
+//print_r($products);
 // Afficher le premier produit de ce tableau
-echo "$products[0]";
+//echo "$products[0]";
 // Afficher le dernier produit de ce tableau
-echo $products[$productsLastIndex];
+//echo $products[$productsLastIndex];
 
+for ($i=0;$i<count($products);$i++){
+   echo $products[$i]."<br>";
+   if ($products[$i] === "iMac") {
+       echo "iMac trouvé !";
+   }
+}
 
 ?>
