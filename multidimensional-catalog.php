580159965b8f576550491b8ec0daaf3fe02700d3
martin brandel <martin.brandel123@gmail.com>
2022-01-28 16:42:42 +0100

    Mise à jour pour Etape 7

--- multidimensional-catalog.php
+++ multidimensional-catalog.php
@@ -1,31 +1,69 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
 <?php
 include 'my-functions.php';
 $products = [
-    "nikeAir270" => [
+    "shoe_1" => [
         "name" => "nikeAir270",
         "price" => 170,
         "weight" => 100,
         "discount" => 20,
-        "picture_url" => "nikeAir270.png",
+        "picture_url" => "image/shoe_1.PNG",
     ],
-    "niketerre270" => [
+    "shoe_2" => [
         "name" => "niketerre270",
-        "price" => 270,
+        "price" => 150,
         "weight" => 220,
-        "discount" => 55,
-        "picture_url" => "niketerre270.png",
+        "discount" => 50,
+        "picture_url" => "image/shoe_2.PNG",
     ],
-    "nikefeu270" => [
+    "shoe_3" => [
         "name" => "nikefeu270",
-        "price" => 1000,
+        "price" => 300,
         "weight" => 62,
         "discount" => 10,
-        "picture_url" => "nikefeu270.png",
+        "picture_url" => "image/shoe_3.PNG",
     ],
 ];
 
-foreach($products as $key => $shoes) { 
-    formatPrice($shoes["price"], $shoes["discount"]);
-      echo "<hr>";
-  }
 ?>
+
+<div class="shoe_container">
+    <?php 
+    foreach($products as $shoe_name => $shoe_caracteristiques) {
+        $img_shoe = $shoe_caracteristiques["picture_url"];
+        echo "<div class='shoe_card'>";
+        echo "<img src='$img_shoe' alt='' class='image_container'>";
+        echo 
+        //formatPrice($shoe_caracteristiques["price"], $shoe_caracteristiques["discount"]);
+        echo "</div>";
+    }
+    ?>
+</div>
+
+
+
+
+
+<br>
+
+
+
+
+
+
+
+
+
+</body>
+</html>
+
+
