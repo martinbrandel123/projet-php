7a09947b41c338f56584c55932bf85e3499b167b
martin brandel <martin.brandel123@gmail.com>
2022-03-10 11:20:29 +0100

    commit projet warrior

---
+++ site_PHP/cookie.php
@@ -0,0 +1,11 @@
+<?php 
+foreach($_POST['quantity'] as $quantity){
+    if(!empty($quantity)){
+        setcookie('quantity',"$quantity", time() + 10);
+    };
+}
+
+Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia facilis consectetur, consequatur non quis, ut consequuntur, eos dolore aliquid saepe voluptates doloremque inventore dolores perspiciatis tempore deleniti. Culpa, itaque asperiores!
+
+
+
